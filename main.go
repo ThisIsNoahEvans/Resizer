847a3802e57d255b930e747e9c6b34ead60fe1b2
Noah Evans <noah@itsnoahevans.co.uk>
2023-11-14 17:06:18 +0000

    Add support for resizing multiple images with multiple sizes

--- main.go
+++ main.go
@@ -16,88 +16,89 @@ import (
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Usage: resizer <image_path> <size> ...")
+		fmt.Println("Usage: resizer <image_path>... <size>...")
 		os.Exit(1)
 	}
 
-	imagePath := os.Args[1]
-	sizes := os.Args[2:]
+	var images []string
+	var sizes [][]uint
 
-	// Open the file.
-	file, err := os.Open(imagePath)
-	if err != nil {
-		fmt.Println("Error opening file:", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	// Decode the image to find out the format.
-	img, format, err := image.Decode(file)
-	if err != nil {
-		fmt.Println("Error decoding image:", err)
-		os.Exit(1)
-	}
-
-	// Seek to the beginning of the file for future reads.
-	if _, err := file.Seek(0, 0); err != nil {
-		fmt.Println("Error seeking file:", err)
-		os.Exit(1)
-	}
-
-	for _, size := range sizes {
-		dimensions := strings.Split(size, "x")
-		var width, height uint
-		var err error
-
-		// Parse width and height.
-		width64, err := strconv.ParseUint(dimensions[0], 10, 64)
-		if err != nil {
-			fmt.Println("Invalid width:", err)
-			continue
-		}
-		width = uint(width64)
-
-		if len(dimensions) == 2 {
-			height64, err := strconv.ParseUint(dimensions[1], 10, 64)
+	// Separate image paths and sizes
+	for _, arg := range os.Args[1:] {
+		if strings.Contains(arg, "x") || (len(arg) == 3 && strings.Contains("0123456789", string(arg[0]))) {
+			// Parse size
+			dimensions := strings.Split(arg, "x")
+			if len(dimensions) != 2 {
+				fmt.Println("Invalid size format:", arg)
+				continue
+			}
+			width, err := strconv.ParseUint(dimensions[0], 10, 64)
+			if err != nil {
+				fmt.Println("Invalid width:", err)
+				continue
+			}
+			height, err := strconv.ParseUint(dimensions[1], 10, 64)
 			if err != nil {
 				fmt.Println("Invalid height:", err)
 				continue
 			}
-			height = uint(height64)
+			sizes = append(sizes, []uint{uint(width), uint(height)})
 		} else {
-			height = width
+			// Assume it's a file path
+			images = append(images, arg)
 		}
+	}
 
-		// Resize the image.
-		m := resize.Resize(width, height, img, resize.Lanczos3)
+	if len(images) == 0 || len(sizes) == 0 {
+		fmt.Println("No valid images or sizes provided.")
+		os.Exit(1)
+	}
 
-		// Create the output file.
-		outputFileName := fmt.Sprintf("%s_%dx%d%s", strings.TrimSuffix(imagePath, filepath.Ext(imagePath)), width, height, filepath.Ext(imagePath))
-		out, err := os.Create(outputFileName)
+	// Process each image with each size
+	for _, imagePath := range images {
+		file, err := os.Open(imagePath)
 		if err != nil {
-			fmt.Println("Error creating file:", err)
+			fmt.Println("Error opening file:", err)
 			continue
 		}
-		defer out.Close()
+		defer file.Close()
 
-		// Encode the image in the original format.
-		switch format {
-		case "jpeg":
-			err = jpeg.Encode(out, m, nil)
-		case "png":
-			err = png.Encode(out, m)
-		case "gif":
-			err = gif.Encode(out, m, nil)
-		default:
-			fmt.Printf("Unsupported image format: %s\n", format)
-			err = fmt.Errorf("unsupported image format: %s", format)
-		}
-
+		img, format, err := image.Decode(file)
 		if err != nil {
-			fmt.Printf("Error encoding image: %s\n", err)
+			fmt.Println("Error decoding image:", err)
 			continue
 		}
 
-		fmt.Printf("Resized image to %dx%d and saved as %s\n", width, height, outputFileName)
+		for _, size := range sizes {
+			width, height := size[0], size[1]
+			m := resize.Resize(width, height, img, resize.Lanczos3)
+
+			outputFileName := fmt.Sprintf("%s_%dx%d%s", strings.TrimSuffix(imagePath, filepath.Ext(imagePath)), width, height, filepath.Ext(imagePath))
+			out, err := os.Create(outputFileName)
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+				continue
+			}
+			defer out.Close()
+
+			switch format {
+			case "jpeg":
+				err = jpeg.Encode(out, m, nil)
+			case "png":
+				err = png.Encode(out, m)
+			case "gif":
+				err = gif.Encode(out, m, nil)
+			default:
+				fmt.Printf("Unsupported image format: %s\n", format)
+				err = fmt.Errorf("unsupported image format: %s", format)
+			}
+
+			if err != nil {
+				fmt.Printf("Error encoding image: %s\n", err)
+				continue
+			}
+
+			fmt.Printf("Resized image to %dx%d and saved as %s\n", width, height, outputFileName)
+		}
 	}
 }
